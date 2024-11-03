968123696f6013376e753004a9063a13fe817c86
Dark_Ville <darkville14@gmail.com>
2024-11-03 13:53:29 +0100

    Create github-replace.js

---
+++ github-replace.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name        GH Markdown to HTML Image
+// @namespace   https://tampermonkey.net/
+// @match       https://*.github.com/*
+// @version     1.0.1
+// @author      Dark_Ville
+// @description replace markdown image with html
+// @grant       GM_addElement
+// ==/UserScript==
+
+function addScript() {
+	const script = `function replaceMD() {
+	let tb = document.querySelector('textarea[id*="issue"]')
+	if (tb) {
+const reg = new RegExp(/!\[Screenshot_[0-9]*\][(]/gm);
+		tb['value'] = tb?.value
+			?.replace(reg, '<img src="')
+			.replace(/[)]/gm, '">');
+	}
+}`;
+	GM_addElement('script', {
+		textContent: script,
+	});
+	setTimeout(addButton, 5000);
+}
+
+function addButton() {
+	const el = document.querySelector('[aria-label="Preview"]');
+	GM_addElement(el, 'button', {
+		class: 'customButton',
+		type: 'button',
+		textContent: 'Change images',
+	});
+	document.querySelector('.customButton').addEventListener('click', replaceMD);
+}
+
+addScript();
+let tb = document.querySelector('.CommentBox-container > textarea');
+if (tb) {
+	tb['value'] = tb?.value
+		?.replace(/!\[Screenshot_([0-9]*)\]\(/gm, '<img src="')
+		.replace(/\)/gm, '">');
+}
